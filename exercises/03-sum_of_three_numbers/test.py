877dc8c431225be7432863903ed2ce9975316616
David Hay <hay.david@hotmail.com>
2019-08-28 00:42:26 +0000

    test

--- exercises/03-sum_of_three_numbers/test.py
+++ exercises/03-sum_of_three_numbers/test.py
@@ -1,9 +1,13 @@
 import io
 import sys
+import os
 sys.stdout = buffer = io.StringIO()
 
 import pytest
 import app
+
+
+
 
 @pytest.mark.it('Create 3 variables each with numbers as value')
 def test_for_variables(capsys):
@@ -11,7 +15,17 @@ def test_for_variables(capsys):
     captured = buffer.getvalue()
     print(captured)
 
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
+    assert app.a in globals()
+
+    #assert type(app.a) is int
+    #assert type(app.b) is int
+    #assert type(app.c) is int
 
 
+@pytest.mark.it('Add all three variables')
+def test_add_variables(capsys):
 
+    captured = buffer.getvalue()
+    bb = captured.rstrip()
+    result = app.a + app.b + app.c
+    assert int(bb) == result
