363d98aca12eb20cfbf71b1d2b974a515a00c1e9
David Hay <hay.david@hotmail.com>
2019-08-29 20:59:12 +0000

    sum

--- exercises/03-sum_of_three_numbers/app.py
+++ exercises/03-sum_of_three_numbers/app.py
@@ -1,3 +1,4 @@
-print("hell")
 
-m = "hello"
+
+
+print()
--- exercises/03-sum_of_three_numbers/test.py
+++ exercises/03-sum_of_three_numbers/test.py
@@ -7,13 +7,19 @@ import pytest
 import app
 
 
-@pytest.mark.it('Create 3 variables each with numbers as value')
+
+@pytest.mark.it('Create 3 variables each with numbers(interger) as value')
 def test_for_file_output(capsys):
 
     captured = buffer.getvalue()
     print(captured)
 
-    assert app.m is not None
+    assert app.a is not None
+    assert type(app.a) is int
+    assert app.b is not None
+    assert type(app.b) is int
+    assert app.c is not None
+    assert type(app.c) is int
 
 
 
@@ -21,6 +27,9 @@ def test_for_file_output(capsys):
 def test_add_variables(capsys):
 
     captured = buffer.getvalue()
-    bb = captured.rstrip()
+    print(captured)
+
     result = app.a + app.b + app.c
-    assert int(bb) == result
+
+    assert captured == str(result) + "\n"
+
