9736d22464b6ba7c96358cfae18ac05ccaba60b0
David Hay <hay.david@hotmail.com>
2019-09-01 01:42:37 +0000

    updated

--- exercises/06-previous_and_next/app.py
+++ exercises/06-previous_and_next/app.py
@@ -1,10 +1,8 @@
 
 #Create a variable named number with any interger as its value
-number = 3
 
 
 
 #Using print, print desired out as shown in the example output
-print("The previous number for the number " + str(number) + " is " + str(number - 1))
-print("The next number for the number " + str(number) + " is " + str(number + 1))
+
 
