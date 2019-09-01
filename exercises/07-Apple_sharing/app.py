2955f1ec9807fb5fb82ee1a8e331e0bc21a1b445
David Hay <hay.david@hotmail.com>
2019-09-01 05:01:46 +0000

    updated

--- exercises/07-Apple_sharing/app.py
+++ exercises/07-Apple_sharing/app.py
@@ -1,11 +1,6 @@
-#Create two variables (n & k) with any interger as its value. 
+#Create two variables (n & k) with any interger as its value.
 #HINT: Import the math module.
-import math
 
-n = 6
-k = 50
 
 
 #Print the two answer per the example output.
-print(math.floor(k / n))
-print(k % n)
