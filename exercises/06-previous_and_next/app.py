eca2c780d204d7626340e84f92b88dd77214320f
David Hay <hay.david@hotmail.com>
2019-09-01 01:41:30 +0000

    previous and next numbers

--- exercises/06-previous_and_next/app.py
+++ exercises/06-previous_and_next/app.py
@@ -5,6 +5,6 @@ number = 3
 
 
 #Using print, print desired out as shown in the example output
-print()
-print("The next number for the number" + str(number) + "is" + str(number + 1))
+print("The previous number for the number " + str(number) + " is " + str(number - 1))
+print("The next number for the number " + str(number) + " is " + str(number + 1))
 
--- exercises/06-previous_and_next/test.py
+++ exercises/06-previous_and_next/test.py
@@ -17,7 +17,9 @@ def test_for_previous_number(capsys):
 
     captured = buffer.getvalue()
     print(captured)
-    assert str(app.number + 1) + "\n" in captured
+    n = app.number
+    a = "The previous number for the number " + str(n) + " is " + str(n - 1)
+    assert str(a) + "\n" in captured
 
 @pytest.mark.it('Print next number')
 def test_for_next_number(capsys):
@@ -25,7 +27,8 @@ def test_for_next_number(capsys):
     captured = buffer.getvalue()
     print(captured)
     n = app.number
-    assert str("The next number for the number" + n + "is" + n + 1) + "\n" in captured
+    a = "The next number for the number " + str(n) + " is " + str(n + 1)
+    assert str(a) + "\n" in captured
 
 
 
