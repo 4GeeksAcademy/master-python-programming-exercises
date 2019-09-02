fa6d763e95d7dafdc98c000af7060e5337236421
David Hay <hay.david@hotmail.com>
2019-09-02 00:39:48 +0000

    minutes and hours

--- exercises/08-hours_and_minutes/app.py
+++ exercises/08-hours_and_minutes/app.py
@@ -1,5 +1,11 @@
-print("dsd")
+#Create three variables (a,b,c). Variable a will have the number of seconds that is passed 
+#since midnight. Variable b will compute the number of hours since midnight and variable c will 
+#compute the number of minutes since midnight. 
 
-def my_function():
-  print("Hello Inside Function")
-  return True
+#hint: import the math module to comute the hours and minutes since midnight. 
+
+
+
+
+#Using print, print the desired output per the example. 
+print()
--- exercises/08-hours_and_minutes/test.py
+++ exercises/08-hours_and_minutes/test.py
@@ -2,26 +2,27 @@ import io
 import sys
 sys.stdout = buffer = io.StringIO()
 
-from app import my_function
+import app
 import pytest
 
-@pytest.mark.it('Your code needs to print hello on the console')
+@pytest.mark.it('Create three variables with the right values')
+def test_for_variables(capsys):
+
+    captured = buffer.getvalue()
+    print(captured)
+
+    assert app.a is not None
+    assert type(app.a) is int
+    assert app.b is not None
+    assert type(app.b) is int
+    assert app.c is not None
+    assert type(app.c) is int
+
+
+@pytest.mark.it('Create three variables with the right values')
 def test_for_file_output(capsys):
 
     captured = buffer.getvalue()
 
+    assert captured == str(app.b) + " " + str(app.c) + "\n"
 
-
-    #print(sys.stdout.read())
-    #print(captured)
-    assert captured == "hello\n" #add \n because the console jumps the line on every print
-
-@pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
-def test_for_function_output(capsys):
-    my_function()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello Inside Function\n"
-
-@pytest.mark.it('Your function needs to return True')
-def test_for_function_return(capsys):
-    assert my_function() == True
