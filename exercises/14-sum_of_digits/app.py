6840f6bb443b7f565b083b98b0a1fbc2646bbe28
David Hay <hay.david@hotmail.com>
2019-09-03 01:28:11 +0000

    update

--- exercises/14-sum_of_digits/app.py
+++ exercises/14-sum_of_digits/app.py
@@ -1,16 +1,17 @@
 #Create two variables("num" and "total"). One variable will have any interger as its value,
 #and the other will have the sum of the digits.
 
-num=int(1234)
+num = 3335
 total=0
+
 while(num>0):
-    dig=num%10
+    dig=num % 10
     total=total+dig
-    num=num//10
-
+    num=num // 10
+    
 #print the sum of the digits
-print("The total sum of digits is:",total)
 
+print(total)
 
 
 
--- exercises/14-sum_of_digits/test.py
+++ exercises/14-sum_of_digits/test.py
@@ -8,7 +8,34 @@ import pytest
 @pytest.mark.it('Create variable "num" and "total"')
 def test_for_variable(capsys):
 
-    captured = buffer.getvalue()
-
     assert app.num is not None
     assert type(app.num) is int
+    assert app.total is not None
+    assert type(app.total) is int
+
+
+@pytest.mark.it('Print the sum of the digits')
+def test_for_sum_of_digits(capsys):
+    captured = buffer.getvalue()
+
+
+    n = app.num
+    tot=0
+
+
+    while(n>0):
+        dig = app.dig
+        tot=tot+dig
+        n = n//10
+
+    assert app.dig == app.num % 10
+    assert captured == str(tot) + "\n"
+
+
+
+
+
+
+
+
+
