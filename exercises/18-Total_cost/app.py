c0d556358615709b7e5bca115b9c0fb6d6e6fefb
David Hay <hay.david@hotmail.com>
2019-09-04 22:03:49 +0000

    total cost

--- exercises/18-Total_cost/app.py
+++ exercises/18-Total_cost/app.py
@@ -1,5 +1,13 @@
-print("dsd")
+#Create three variables (a,b,n), and give them any interger as its value.
 
-def my_function():
-  print("Hello Inside Function")
-  return True
+N = int(input("Number of cupcakes:"))
+A = int(input("Cup cake price dollar amt:"))
+B = int(input("Cup cake price cents amt:"))
+Total_cost_dollar= (N*A)
+Total_cost_cents= (N*B)
+print(Total_cost_dollar,Total_cost_cents)
+
+
+
+#Print the total cost in dollar and cents.
+print()
--- exercises/18-Total_cost/test.py
+++ exercises/18-Total_cost/test.py
@@ -2,10 +2,10 @@ import io
 import sys
 sys.stdout = buffer = io.StringIO()
 
-from app import my_function
+import app
 import pytest
 
-@pytest.mark.it('Your code needs to print hello on the console')
+@pytest.mark.it('Create three variables(a,b,n)')
 def test_for_file_output(capsys):
 
     captured = buffer.getvalue()
@@ -16,12 +16,4 @@ def test_for_file_output(capsys):
     #print(captured)
     assert captured == "hello\n" #add \n because the console jumps the line on every print
 
-@pytest.mark.it('Your function needs to print "Hello Inside Function" on the console')
-def test_for_function_output(capsys):
-    my_function()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello Inside Function\n"
 
-@pytest.mark.it('Your function needs to return True')
-def test_for_function_return(capsys):
-    assert my_function() == True
