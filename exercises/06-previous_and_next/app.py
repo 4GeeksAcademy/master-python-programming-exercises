303c8eb295fdb88ff56316f1c76c9e6b4bd97e37
David Hay <hay.david@hotmail.com>
2019-09-08 23:53:50 +0000

    test done

--- exercises/06-previous_and_next/app.py
+++ exercises/06-previous_and_next/app.py
@@ -1,11 +1,14 @@
+#Create three variables : "num","previousn" and "nextn".
+# var n will have any interger as its value.
+# var previousn will contain the previous number.
+# var nextn will contain the next number.
 
-#Create a variable named "number" with any interger as its value
-number = 80
-previousn = number - 1
-nextn = number + 1
+num = 80
+previousn = num - 1
+nextn = num + 1
 
 
 
-#print desired out as shown in the example output.
-print(previousn)
-print(nextn)
+#Using the variables above, print desired output as shown in the example.
+print("The next number for the number", num, "is", nextn)
+print("The previous number for the number",num,"is",previousn)
--- exercises/06-previous_and_next/test.py
+++ exercises/06-previous_and_next/test.py
@@ -1,5 +1,7 @@
 import io
 import sys
+import os
+import re
 sys.stdout = buffer = io.StringIO()
 
 import app
@@ -8,8 +10,8 @@ import pytest
 @pytest.mark.it('Create variable named number with any int as its value')
 def test_for_variable(capsys):
 
-    assert app.number is not None
-    assert type(app.number) is int
+    assert app.num is not None
+    assert type(app.num) is int
 
 
 @pytest.mark.it('Print previous number')
@@ -17,20 +19,54 @@ def test_for_previous_number(capsys):
 
     captured = buffer.getvalue()
     print(captured)
-    n = app.number
+    n = app.num
     a = "The previous number for the number " + str(n) + " is " + str(n - 1)
     assert str(a) + "\n" in captured
+
+
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.read()
+    print(content)
+    ab =0
+    text = content
+    for m in re.finditer('num', text):
+        #print(len(m))
+        ab += 1
+        print('num found', m.start(), m.end(), ab)
+
+    if ab < 12:
+        assert False
+
+    pre = 0
+    for m in re.finditer('previousn', text):
+        pre += 1
+        print('previousnn found', m.start(), m.end(), pre)
+
+    if pre < 4:
+        assert False
+
 
 @pytest.mark.it('Print next number')
 def test_for_next_number(capsys):
 
     captured = buffer.getvalue()
     print(captured)
-    n = app.number
+    n = app.num
     a = "The next number for the number " + str(n) + " is " + str(n + 1)
     assert str(a) + "\n" in captured
 
+    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    content = f.read()
+    print(content)
+    ab =0
+    text = content
+    for m in re.finditer('num', text):
+        #print(len(m))
+        ab += 1
+        print('num found', m.start(), m.end(), ab)
 
+    if ab < 12:
+        assert False
 
 
 
