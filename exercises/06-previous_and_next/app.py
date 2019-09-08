f902da8dfdda8f9de6a09b8e4da580bd7c41e005
David Hay <hay.david@hotmail.com>
2019-09-08 01:47:01 +0000

    updated exercises

--- exercises/06-previous_and_next/app.py
+++ exercises/06-previous_and_next/app.py
@@ -1,8 +1,11 @@
 
-#Create a variable named number with any interger as its value
+#Create a variable named "number" with any interger as its value
+number = 80
+previousn = number - 1
+nextn = number + 1
 
 
 
-#Using print, print desired out as shown in the example output.
-print()
-print()
+#print desired out as shown in the example output.
+print(previousn)
+print(nextn)
