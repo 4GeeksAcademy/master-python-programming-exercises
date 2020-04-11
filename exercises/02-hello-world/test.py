ecb21b03afab557b21005ec013c58efd96fc4282
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-04-11 19:05:26 +0000

    updated #02

--- exercises/02-hello-world/test.py
+++ exercises/02-hello-world/test.py
@@ -1,7 +1,7 @@
 import io, sys, pytest, mock
 
 @pytest.mark.it('Your code needs to print Hello World on the console')
-def test_for_file_output():
-    sys.stdout = buffer = io.StringIO()
+def test_for_file_output(capsys):
     import app
-    assert buffer.getvalue() == "Hello World\n" #add \n because the console jumps the line on every print
+    captured = capsys.readouterr()
+    assert "Hello World\n" == captured.out
