1b96993cd63d66fa5790ac6c8131296f75afb00e
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-01-25 16:32:07 -0400

    Update app.py

--- exercises/11-Swap_digits/app.py
+++ exercises/11-Swap_digits/app.py
@@ -1,11 +1,8 @@
 #Complete the fuction to return the swapped digits of a given two-digit-interger.
 def swap_digits(num):
-  result = str(num%10)+str(num//10)
-  return (result)
+  # Your code here
    
    
    
 #Invoke the function with any two digit interger as its argument
 print(swap_digits(76))
-
-
