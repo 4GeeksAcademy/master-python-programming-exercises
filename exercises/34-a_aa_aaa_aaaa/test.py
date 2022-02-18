1e56c0ca2ca260fd08e062b12953c772c8b8a7f6
Martín Suárez <ms.prgramador@gmail.com>
2022-02-18 18:52:22 +0000

    34-a_aa_aaa_aaaa

---
+++ exercises/34-a_aa_aaa_aaaa/test.py
@@ -0,0 +1,10 @@
+import io, sys, pytest, os, re, mock
+
+@pytest.mark.it('The solution should return the expected output')
+def test_convert_inputs(capsys, app):
+
+    fake_input = ["9"] #fake input
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out != "11106\n"
