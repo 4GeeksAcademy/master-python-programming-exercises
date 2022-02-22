d8caa138b7efa955651414059483de57a96aa667
Martín Suárez <ms.prgramador@gmail.com>
2022-02-22 14:20:33 +0000

    04-area_of_right_triangle

--- exercises/04-area_of_right_triangle/test.py
+++ exercises/04-area_of_right_triangle/test.py
@@ -13,5 +13,12 @@ def test_area_of_triangle(stdin):
     result = int(_stdin[0]) * int(_stdin[1]) / 2
     assert buffer.getvalue() == str(result) + "\n"
 
-
+@pytest.mark.it('The function area_of_triangle should exist')
+def test_function_exists(capsys):
+  
+  try:
+    import app
+    app.area_of_triangle
+  except AttributeError:
+      raise AttributeError("The function 'area_of_triangle' should exist on app.py")
 
