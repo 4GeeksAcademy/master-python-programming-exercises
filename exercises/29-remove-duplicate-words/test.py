a74ba7f75a38a32a822b7222bf8dafd6a4f61ab3
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 13:38:03 +0000

    38-sort-tuples-ascending

--- exercises/29-remove-duplicate-words/test.py
+++ exercises/29-remove-duplicate-words/test.py
@@ -1,9 +1,9 @@
 import pytest, io, sys, json, mock, re, os
 
-@pytest.mark.it('')
+@pytest.mark.it('Your solution should work as expected')
 def test_expected_output(capsys, app):
     fake_input=['Hola como Hola']
     with mock.patch('builtins.input', lambda x: fake_input.pop()):
         app()
         captured = capsys.readouterr()
-        assert captured.out != " como \n"
+        assert captured.out == " como \n"
