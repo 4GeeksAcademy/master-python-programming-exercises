1619ee44e860a7485a53db1d7a8a1f966b577138
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 20:53:15 +0000

    22-Integral

--- exercises/22-Integral/test.py
+++ exercises/22-Integral/test.py
@@ -1,27 +1,23 @@
-import pytest,os,re,io,sys, mock, json, unittest
-from unittest.mock import patch
+import pytest,os,re,io,sys, mock, json
 
 
-# @pytest.mark.it('The solution should work with other entries')
-# def test_expected_output_5(stdin):
+@pytest.mark.it('Almost there! But you need to convert the incoming input from string to integer')
+def test_convert_inputs(capsys, app):
 
-#     _stdin = ['5']
-#     with mock.patch('builtins.input', lambda x: _stdin.pop(0)):
+    fake_input = ["8"] #fake input
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out == "{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}\n"
 
-#         sys.stdout = buffer = io.StringIO()
-#         import app
-#         # _stdin = json.loads(stdin)
-#         result = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
-#         assert buffer.getvalue() == str(result) + "\n"
 
-@pytest.mark.it('The solution should return the expected output')
-def test_expected_output_8(stdin):
+@pytest.mark.it('Almost there! But you need to convert the incoming input from string to integer')
+def test_convert_inputs_2(capsys, app):
 
-    _stdin = ['8']
-    with mock.patch('builtins.input', lambda x: _stdin.pop(0)):
+    fake_input = ["5"] #fake input
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out == "{1: 1, 2: 4, 3: 9, 4: 16, 5: 25}\n"
 
-        sys.stdout = buffer = io.StringIO()
-        import app
-        # _stdin = json.loads(stdin)
-        result = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
-        assert buffer.getvalue() == str(result) + "\n"
+
