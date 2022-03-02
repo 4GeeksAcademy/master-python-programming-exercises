64b440b914fdae2ab8f3e4d0d3233cb01e1d911a
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 13:46:11 +0000

    38-sort-tuples-ascending

---
+++ exercises/38-sort-tuples-ascending/test.py
@@ -0,0 +1,12 @@
+import pytest, io, sys, json, mock, re, os
+
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+
+@pytest.mark.it('The solution should return the expected output')
+def test_convert_inputs(capsys, app):
+
+    fake_input = ["Tom,19,80 John,20,90 Jony,17,91 Jony,17,93 Json,21,85"] #fake input
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out == "[('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]\n"
