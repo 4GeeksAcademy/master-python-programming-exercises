c9e120af149821d24780f8c37925b7f5a5710686
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 15:39:29 +0000

    04-area_of_right_triangle

--- exercises/04-area_of_right_triangle/test.py
+++ exercises/04-area_of_right_triangle/test.py
@@ -1,5 +1,13 @@
 import io, sys, os, pytest, json, mock
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+@pytest.mark.it('The function area_of_triangle should exist')
+def test_function_exists(capsys):
+  
+  try:
+    import app
+    app.area_of_triangle
+  except AttributeError:
+      raise AttributeError("The function 'area_of_triangle' should exist on app.py")
 
 @pytest.mark.it('Calculate the area of the triangle.')
 def test_area_of_triangle(stdin):
@@ -13,12 +21,4 @@ def test_area_of_triangle(stdin):
     result = int(_stdin[0]) * int(_stdin[1]) / 2
     assert buffer.getvalue() == str(result) + "\n"
 
-@pytest.mark.it('The function area_of_triangle should exist')
-def test_function_exists(capsys):
-  
-  try:
-    import app
-    app.area_of_triangle
-  except AttributeError:
-      raise AttributeError("The function 'area_of_triangle' should exist on app.py")
 
