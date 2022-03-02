8a2ca9ec64b5ec76ee608661866327fc269be299
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 17:33:51 +0000

    38-sort-tuples-ascending

--- exercises/38-sort-tuples-ascending/solution.hide.py
+++ exercises/38-sort-tuples-ascending/solution.hide.py
@@ -2,9 +2,9 @@ from operator import itemgetter, attrgetter
 
 l = []
 while True:
-    s = raw_input()
+    s = input("")
     if not s:
         break
     l.append(tuple(s.split(",")))
 
-print sorted(l, key=itemgetter(0,1,2))
+print (sorted(l, key=itemgetter(0,1,2)))
