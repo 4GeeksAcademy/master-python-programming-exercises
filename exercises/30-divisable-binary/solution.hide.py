22abad5948a339a7994b945a0ae3f5364618b05d
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 16:45:09 +0000

    30-divisable-binary

--- exercises/30-divisable-binary/solution.hide.py
+++ exercises/30-divisable-binary/solution.hide.py
@@ -1,5 +1,5 @@
 value = []
-items=[x for x in input().split(',')]
+items=[x for x in input("").split(',')]
 for p in items:
     intp = int(p, 2)
     if not intp%5:
--- exercises/30-divisable-binary/test.py
+++ exercises/30-divisable-binary/test.py
@@ -10,8 +10,8 @@ def test_expected_output(capsys, app):
 
 @pytest.mark.it('Your solution should work with other parameters')
 def test_expected_output_2(capsys, app):
-    fake_input=['0100,0011,1010,1001']
+    fake_input=['1111,1000,0101,0000']
     with mock.patch('builtins.input', lambda x: fake_input.pop()):
         app()
         captured = capsys.readouterr()
-        assert captured.out == "1111,0000,0101\n"
+        assert captured.out == "1111,0101,0000\n"
