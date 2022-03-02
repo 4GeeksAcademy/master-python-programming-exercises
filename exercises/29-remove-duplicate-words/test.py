5abacb13184c2c2dc9ca30fdb5a7b89a8ec8dc43
Martín Suárez <ms.prgramador@gmail.com>
2022-03-02 16:41:04 +0000

    29-remove-duplicate-words

--- exercises/29-remove-duplicate-words/test.py
+++ exercises/29-remove-duplicate-words/test.py
@@ -6,4 +6,21 @@ def test_expected_output(capsys, app):
     with mock.patch('builtins.input', lambda x: fake_input.pop()):
         app()
         captured = capsys.readouterr()
-        assert captured.out == " como \n"
+        assert captured.out == "Hola como\n"
+
+@pytest.mark.it('Your solution should work as expected')
+def test_expected_output_2(capsys, app):
+    fake_input=['ayer como ayer es hoy']
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out == "ayer como es hoy\n"
+
+
+@pytest.mark.it('Your solution should work as expected')
+def test_expected_output_no_repetead(capsys, app):
+    fake_input=['Hola como estas']
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+        app()
+        captured = capsys.readouterr()
+        assert captured.out == "Hola como estas\n"
