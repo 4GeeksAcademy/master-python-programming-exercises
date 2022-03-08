06d60b69425f372c8a8f24dcbbd910510e9330af
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-03-08 16:57:11 -0400

    Update test.py

--- exercises/08-hours_and_minutes/test.py
+++ exercises/08-hours_and_minutes/test.py
@@ -13,12 +13,12 @@ def test_for_file_output(capsys, app):
     assert app.hours_minutes(3900) == (1, 5)
 
 
-@pytest.mark.it('The function hours_minutes must return the correct output for 4004 secs')
+@pytest.mark.it('The function hours_minutes must return the correct output for 4500 secs')
 def test_for_file_output(capsys, app):
     assert app.hours_minutes(4500) == (1, 15)
 
 
-@pytest.mark.it('The function hours_minutes must return the correct output for 60 secs')
+@pytest.mark.it('The function hours_minutes must return the correct output for 7320 secs')
 def test_for_file_output(capsys, app):
     assert app.hours_minutes(7320) == (2, 2)
 
