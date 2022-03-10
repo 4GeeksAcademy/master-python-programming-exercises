8615164b09d951bfbd52a3e0d065fb5175850266
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-03-10 22:40:18 +0000

    Added solution.hide.py of exercise 25.

--- exercises/25-print-formula/solution.hide.py
+++ exercises/25-print-formula/solution.hide.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
 import math
-def print_formula(n1,n2,n3):
-    value = []
-    items=[x for x in "{},{},{}".format(n1,n2,n3).split(',')]
-    for d in items:
-        value.append(str(int(round(math.sqrt(2*50*float(d)/30)))))
+def print_formula(num):
+    return round(math.sqrt(2*50*num/30))
 
-    return (','.join(value))
+print(print_formula(5))
