4faba1a7d3f23e1d3da85eb1d743c97c5c0dfab2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 00:45:53 +0100

    Update solution.hide.py

--- exercises/013-sum_of_digits/solution.hide.py
+++ exercises/013-sum_of_digits/solution.hide.py
@@ -1,11 +1,10 @@
-#Complete the function "digits_sum" so that it prints the sum of a three digit number.
+# Complete the function "digits_sum" so that it prints the sum of a three-digit number
 def digits_sum(num):
   aux = 0
   for x in str(num):
-    aux= aux+int(x)
+    aux = aux + int(x)
   return aux
 
 
-#Invoke the function with any three-digit-number
-#You can try other three-digit numbers if you want
+# Invoke the function with any three-digit number
 print(digits_sum(123))
