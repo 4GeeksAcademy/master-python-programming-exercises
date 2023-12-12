065219c8a4cfbfee5e8713854369d5ba605d288c
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 17:55:15 +0100

    Update solution.hide.py

--- exercises/005-previous_and_next/solution.hide.py
+++ exercises/005-previous_and_next/solution.hide.py
@@ -4,5 +4,5 @@ def previous_next(num):
   return (num-1, num+1)
 
 
-# Invoke the function with any integer as its argument.
+# Invoke the function with any integer as its argument
 print(previous_next(179))
