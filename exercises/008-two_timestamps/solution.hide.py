73fb1b7933d8eaf5ed50ec54dc30ac4d8d1fff5b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 19:31:18 +0100

    Update solution.hide.py

--- exercises/008-two_timestamps/solution.hide.py
+++ exercises/008-two_timestamps/solution.hide.py
@@ -1,14 +1,13 @@
-def two_timestamp(hr1,min1,sec1,hr2,min2,sec2):
+def two_timestamp(hr1, min1, sec1, hr2, min2, sec2):
     # Your code here   
-    fisrt_hour = hr1 * 3600
+    first_hour = hr1 * 3600
     first_min = min1 * 60
-    final_first = fisrt_hour + first_min + sec1
+    final_first = first_hour + first_min + sec1
     second_hour = hr2 * 3600
     second_min = min2 * 60
     final_second = second_hour + second_min + sec2
     
     return final_second - final_first
 
-
-# Invoke the function and pass two timestamps(6 intergers) as its arguments
-print(two_timestamp(1,1,1,2,2,2))
+# Invoke the function and pass two timestamps(6 integers) as its arguments
+print(two_timestamp(1, 1, 1, 2, 2, 2))
