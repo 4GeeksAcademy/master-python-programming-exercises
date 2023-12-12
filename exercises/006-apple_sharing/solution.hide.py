f8471ab2dfdfebacd49c19394daacaf5af0f5db0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 18:10:44 +0100

    Update solution.hide.py

--- exercises/006-apple_sharing/solution.hide.py
+++ exercises/006-apple_sharing/solution.hide.py
@@ -1,12 +1,6 @@
-#Complete the function to return:
-#1) How many apples each single student will get.
-#2) How many apples wil remain in the basket.
-#Hint: You can resolve this exercise either importing the math module or without it 
 def apple_sharing(n,k):
-  
+  # Your code here
   return (round(k/n),k%n)
  
-    
 
-#Print the two answer per the example output.
 print(apple_sharing(6,50))
