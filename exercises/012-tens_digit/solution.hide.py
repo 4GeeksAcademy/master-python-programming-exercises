20360fba95dad2cfd96724cecd77515cf579f139
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 21:49:27 +0100

    Update solution.hide.py

--- exercises/012-tens_digit/solution.hide.py
+++ exercises/012-tens_digit/solution.hide.py
@@ -1,9 +1,7 @@
-#Complete the function to return the tens digit of a given interger
+# Complete the function to return the tens digit of a given integer
 def tens_digit(num):
   return (num // 10) % 10
 
 
-
-
-#Invoke the function with any interger.
-print(tens_digit(123))
+# Invoke the function with any integer
+print(tens_digit())
