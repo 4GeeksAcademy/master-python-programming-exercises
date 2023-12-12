bf5ce848476ae303a899e94363f9dc16aae28a22
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 19:16:35 +0100

    Update solution.hide.py

--- exercises/007-hours_and_minutes/solution.hide.py
+++ exercises/007-hours_and_minutes/solution.hide.py
@@ -1,8 +1,8 @@
-def hours_minutes(secs):
+def hours_minutes(seconds):
   # Your code here
-  minutes = secs // 60
-  hours = minutes // 60
-  minutes = minutes % 60
+  hours = seconds // 3600
+  remaining_seconds = seconds % 3600
+  minutes = remaining_seconds // 60
   return (hours, minutes)
 
 # Invoke the function and pass any integer as its argument
