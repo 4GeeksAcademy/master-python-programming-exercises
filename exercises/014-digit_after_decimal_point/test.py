b4a206918be139006467154d70ab76a912eae270
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 00:56:47 +0100

    Update test.py

--- exercises/014-digit_after_decimal_point/test.py
+++ exercises/014-digit_after_decimal_point/test.py
@@ -1,6 +1,6 @@
 import io, sys, pytest, os, re, mock
 
-@pytest.mark.it('The function  first_digits must exist')
+@pytest.mark.it('The function first_digit must exist')
 def test_for_functon_existence(capsys, app):
     assert callable(app.first_digit)
 
@@ -12,7 +12,7 @@ def test_for_return(capsys, app):
 def test_for_output_type(capsys, app):
     assert type(app.first_digit(1.2)) == type(1)
 
-@pytest.mark.it('We tried to pass 6.24 as parameter and it did not return 2!')
+@pytest.mark.it('We tried to pass 6.24 as parameter and it did not return 2')
 def test_for_file_output(capsys, app):
     assert app.first_digit(6.24) == int(6.24 *10)%10
 
