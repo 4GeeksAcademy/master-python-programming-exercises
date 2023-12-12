d15a5eda523dbc309ec122821edba9b8effc0278
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-12 21:15:31 +0100

    Update solution.hide.py

--- exercises/010-swap_digits/solution.hide.py
+++ exercises/010-swap_digits/solution.hide.py
@@ -1,10 +1,8 @@
-#Complete the fuction to return the swapped digits of a given two-digit-interger.
+# Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-  aux = str(num)[1] +str(num)[0]
-  
+  aux = str(num)[1] + str(num)[0]
   return int(aux)
-  
    
-#Invoke the function with any two digit interger as its argument
+# Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
