d54fd70d9d99d7fac62cc2d66fc532fb4e3e94db
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 03:57:05 +0100

    Update solution.hide.py

--- exercises/017-total_cost/solution.hide.py
+++ exercises/017-total_cost/solution.hide.py
@@ -1,6 +1,9 @@
 # Complete the function to return the total cost in dollars and cents of (n) cupcakes
-def total_cost(d,c,n):
-    return ((((c*100)+c)*n)//100, (((d*100)+c)*n)%100)
+def total_cost(d, c, n):
+    total_cents = (d * 100 + c) * n
+    total_dollars = total_cents // 100
+    remaining_cents = total_cents % 100
+    return total_dollars, remaining_cents
 
 
 # Invoke the function with three integers: total_cost(dollars, cents, number of cupcakes)
