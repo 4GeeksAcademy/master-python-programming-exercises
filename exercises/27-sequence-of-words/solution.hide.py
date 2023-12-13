eb9ff26840fee2e19ac079e2fc74c69d6c981faf
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 17:37:58 +0100

    Update solution.hide.py

--- exercises/27-sequence-of-words/solution.hide.py
+++ exercises/27-sequence-of-words/solution.hide.py
@@ -1,4 +1,7 @@
+# Your code here
 def sequence_of_words(words):
     items=[x for x in "{}".format(words).split(',')]
     items.sort()
     return (','.join(items))
+
+print(sequence_of_words("this,is,sorted"))
