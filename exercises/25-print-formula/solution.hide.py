46581d67421c844ac49e8bd3df5ae78a1cd81dbd
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 13:52:57 +0100

    Update solution.hide.py

--- exercises/25-print-formula/solution.hide.py
+++ exercises/25-print-formula/solution.hide.py
@@ -1,6 +1,6 @@
 import math
 
-def print_formula(num):
-    return round(math.sqrt(2 * 50 * num / 30))
+def print_formula(d):
+    return round(math.sqrt(2 * 50 * d / 30))
 
 print(print_formula(150))
