d4f674590b3d7ccc05879942db84ae95721b1f5d
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 01:23:16 +0100

    Update app.py

--- exercises/016-century/app.py
+++ exercises/016-century/app.py
@@ -1,10 +1,7 @@
-#Complete the function to return the respective number of the century
-#HINT: You may need to import the math module.
-
+# Complete the function to return the respective number of the century
 def century(year):
   return None
 
 
-
-#Invoke the function with any given year. 
+# Invoke the function with any given year
 print(century())
