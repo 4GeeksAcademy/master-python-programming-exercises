829f39610cbd4df4cf2cf8ee8e6921ec6ad263f4
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 16:52:07 +0100

    Update test.py

--- exercises/26-two-dimensional-array/test.py
+++ exercises/26-two-dimensional-array/test.py
@@ -8,10 +8,10 @@ def test_function_existence(capsys, app):
 def test_expected_output(capsys, app):
     assert app.two_dimensional_list(3,5) == [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
 
-@pytest.mark.it('The function should work with other entries. Testing with 2, 7')
+@pytest.mark.it('The function should work with other entries. Testing with (2, 7)')
 def test_expected_output(capsys, app):
     assert app.two_dimensional_list(2,7) == [[0, 0, 0, 0, 0, 0, 0], [0, 1, 2, 3, 4, 5, 6]]
         
-@pytest.mark.it('The function should work with other entries. Testing with 1, 10')
+@pytest.mark.it('The function should work with other entries. Testing with (1, 10)')
 def test_expected_output(capsys, app):
     assert app.two_dimensional_list(1,10) == [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
