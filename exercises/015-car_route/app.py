f50f10dc69cc9baf0e25e00ca05cf699e27498df
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 01:09:46 +0100

    Update app.py

--- exercises/015-car_route/app.py
+++ exercises/015-car_route/app.py
@@ -1,9 +1,7 @@
-#Complete the function to return the amount of days it will take to cover a route.
-#HINT: You may need to import the math module for this exercise.
-
+# Complete the function to return the amount of days it will take to cover a route
 def car_route(n,m):
   return None
 
 
-#Invoke the function with two intergers.
+# Invoke the function with two integers
 print(car_route())
