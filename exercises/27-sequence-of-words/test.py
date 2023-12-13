e84c78a27833265281b4e8784ed3365219350945
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 17:18:22 +0100

    Update test.py

--- exercises/27-sequence-of-words/test.py
+++ exercises/27-sequence-of-words/test.py
@@ -9,6 +9,6 @@ def test_function_existence(capsys, app):
 def test_function_existence(capsys, app):
     assert app.sequence_of_words("bag,hello,without,world") == "bag,hello,without,world"
 
-@pytest.mark.it('The function should return the expected output')
+@pytest.mark.it('The function should return the expected output. Testing with different values')
 def test_function_existence(capsys, app):
     assert app.sequence_of_words("No,pain,gain") == "No,gain,pain"
