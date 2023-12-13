2e9fd8a52886c6bf6ae96d6f7f42a8abb7719fe8
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 06:17:20 +0100

    Update solution.hide.py

--- exercises/23-list-and-tuple/solution.hide.py
+++ exercises/23-list-and-tuple/solution.hide.py
@@ -1,10 +1,10 @@
-values=input("")
-l=values.split(",")
-t=tuple(l)
-print (l)
-print (t)
+# Your code here
+def list_and_tuple(*args):
+    string_list = [str(num) for num in args]
+    string_tuple = tuple(string_list)
+    
+    return string_list, string_tuple
 
-# 
-values=input("")
-t=tuple(values.split(','))
-print(t)
+result_list, result_tuple = list_and_tuple(1, 2, 3, 4, 5)
+print(result_list)
+print(result_tuple)
