db4ea9e379a2e4353ab0a004888ebd979b7142e0
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-13 01:28:59 +0100

    Update solution.hide.py

--- exercises/016-century/solution.hide.py
+++ exercises/016-century/solution.hide.py
@@ -3,9 +3,9 @@ import math
 # Complete the function to return the respective number of the century
 def century(year):
   if year % 100 == 0:
-    return math.floor(year/100) 
+    return math.floor(year / 100) 
   else:
-    return math.floor(year/100 +1)
+    return math.floor(year / 100 + 1)
 
 
 # Invoke the function with any given year
