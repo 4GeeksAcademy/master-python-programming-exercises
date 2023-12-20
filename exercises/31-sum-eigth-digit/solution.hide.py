a99592312e18fdc7c7599387654d2932fa63e5a6
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 11:21:47 +0100

    Update solution.hide.py

--- exercises/31-sum-eigth-digit/solution.hide.py
+++ exercises/31-sum-eigth-digit/solution.hide.py
@@ -1,6 +1,11 @@
-values = []
-for i in range(1000, 3001):
-    s = str(i)
-    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
-        values.append(s)
-print ",".join(values)
+# Your code here
+def sum_eight_digit():
+    values = []
+    for i in range(1000, 3001):
+        s = str(i)
+        if (int(s[0]) % 2 == 0) and (int(s[1]) % 2 == 0) and (int(s[2]) % 2 == 0) and (int(s[3]) % 2 == 0):
+            values.append(s)
+
+    return ",".join(values)
+
+print(sum_eight_digit())
