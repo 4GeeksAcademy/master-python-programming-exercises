ec7c4043bedb71391e0654d7a971cdcc6d3c5b65
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 13:59:34 +0100

    Update solution.hide.py

--- exercises/36-net-amount/solution.hide.py
+++ exercises/36-net-amount/solution.hide.py
@@ -1,9 +1,12 @@
+# Your code here
 def net_amount(param):
-    netAmount = 0
+    total = 0
     values = param.split()
     for x in range(len(values)):
         if values[x] == 'D':
-            netAmount+=int(values[x+1])
+            total+=int(values[x+1])
         elif values[x] == 'W':
-            netAmount-=int(values[x+1])
-    return netAmount
+            total-=int(values[x+1])
+    return total
+
+print(net_amount("D 300 W 200 D 400"))
