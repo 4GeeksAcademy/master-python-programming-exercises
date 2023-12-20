8e73ab03fc656c1c6290852ed5fc58c2a368cc8e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 12:41:42 +0100

    Update solution.hide.py

--- exercises/34-a_aa_aaa_aaaa/solution.hide.py
+++ exercises/34-a_aa_aaa_aaaa/solution.hide.py
@@ -1,6 +1,9 @@
+# Your code here
 def computed_value(param):
     n1 = int( "%s" % param )
     n2 = int( "%s%s" % (param,param) )
     n3 = int( "%s%s%s" % (param,param,param) )
     n4 = int( "%s%s%s%s" % (param,param,param,param) )
     return (n1+n2+n3+n4)
+
+print(computed_value(9))
