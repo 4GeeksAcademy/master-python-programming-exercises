ec5641ce2f6b47189c420c111a87165b900305a9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 13:24:58 +0100

    Update solution.hide.py

--- exercises/35-square-each-odd-number/solution.hide.py
+++ exercises/35-square-each-odd-number/solution.hide.py
@@ -1,3 +1,8 @@
-values = raw_input()
-numbers = [x for x in values.split(",") if int(x)%2!=0]
-print ",".join(numbers)
+# Your code here
+def square_odd_numbers(numbers):
+    number_list = [int(num) for num in numbers.split(',')]
+    squared_odd_numbers = [num**2 for num in number_list if num % 2 != 0]
+
+    return squared_odd_numbers
+
+print(square_odd_numbers("1,2,3,4,5,6,7"))
