459bc2fcd226dc8871f8becd2bbe5bba963f2329
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 15:18:39 +0100

    Update solution.hide.py

--- exercises/37-validity-of-password/solution.hide.py
+++ exercises/37-validity-of-password/solution.hide.py
@@ -1,4 +1,5 @@
 import re
+
 def valid_password(password):
     value = []
     items=[x for x in password.split(',')]
@@ -21,3 +22,5 @@ def valid_password(password):
             pass
         value.append(p)
     return (",".join(value))
+
+print(valid_password("ABd1234@1,ABd1234@1,2w3E*,2We3345"))
