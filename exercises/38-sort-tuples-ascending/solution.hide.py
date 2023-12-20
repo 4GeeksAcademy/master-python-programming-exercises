d40a9ad8caef492217bad8e697636f09c2f7b8c3
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-20 15:49:07 +0100

    Update solution.hide.py

--- exercises/38-sort-tuples-ascending/solution.hide.py
+++ exercises/38-sort-tuples-ascending/solution.hide.py
@@ -1,5 +1,10 @@
-from operator import itemgetter, attrgetter
+from operator import itemgetter
+
 def sort_tuples_ascending(tuples):
-    l = []
-    l.append(tuple(tuples.split(",")))
-    return (sorted(l, key=itemgetter(0,1,2)))
+    tuple_list = [tuple(item.split(',')) for item in tuples.split()]
+
+    sorted_list = sorted(tuple_list, key=itemgetter(0, 1, 2))
+
+    return sorted_list
+
+print(sort_tuples_ascending("Tom,19,80 John,20,90 Jony,17,91 Jony,17,93 Jason,21,85"))
