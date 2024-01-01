c6a52ad0f8351ca35506afb5902e850db79b18b9
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-01 03:13:15 +0100

    Update test.py

--- exercises/37-validity-of-password/test.py
+++ exercises/37-validity-of-password/test.py
@@ -8,12 +8,12 @@ def test_function_existence(capsys, app):
 
 @pytest.mark.it('The function should return the expected output')
 def test_expected_output(capsys, app):
-    assert app.valid_password("ABd1234@1,a F1#,2w3E*,2We3345") == "ABd1234@1"
+    assert app.valid_password("ABd1234@1") == "Valid password"
 
 @pytest.mark.it('Your solution should work as expected for valid passwords')
 def test_expected_another_output(capsys, app):
-    assert app.valid_password("Lmd4567@2,a F1#,2w3E*,2We3345") == "Lmd4567@2"
+    assert app.valid_password("Lmd4567@2") == "Valid password"
 
 @pytest.mark.it('Your solution should work as expected when there is no valid password input')
 def test_expected_output_no_valid_entries(capsys, app):
-    assert app.valid_password("ABd12,a F1#,2w3E*,2We3345") == ""
+    assert app.valid_password("ABd12") == "Invalid password. Please try again"
