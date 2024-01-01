041dc4b16a3f4e75ddc61ae1c421a26c3e8c267d
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-01 03:11:15 +0100

    Update solution.hide.py

--- exercises/37-validity-of-password/solution.hide.py
+++ exercises/37-validity-of-password/solution.hide.py
@@ -2,26 +2,35 @@
 import re
 
 def valid_password(password):
-    value = []
-    items=[x for x in password.split(',')]
-    for p in items:
-        if len(p)<6 or len(p)>12:
-            continue
-        else:
-            pass
-        if not re.search("[a-z]",p):
-            continue
-        elif not re.search("[0-9]",p):
-            continue
-        elif not re.search("[A-Z]",p):
-            continue
-        elif not re.search("[$#@]",p):
-            continue
-        elif re.search("\s",p):
-            continue
-        else:
-            pass
-        value.append(p)
-    return (",".join(value))
+    if len(password) < 6 or len(password) > 12:
+        return "Invalid password. Please try again"
 
-print(valid_password("ABd1234@1,ABd1234@1,2w3E*,2We3345"))
+    if not re.search("[a-z]", password):
+        return "Invalid password. Please try again"
+    elif not re.search("[0-9]", password):
+        return "Invalid password. Please try again"
+    elif not re.search("[A-Z]", password):
+        return "Invalid password. Please try again"
+    elif not re.search("[$#@]", password):
+        return "Invalid password. Please try again"
+    elif re.search("\s", password):
+        return "Invalid password. Please try again"
+
+    return "Valid password"
+
+print(valid_password("ABd1234@1"))
+
+
+### SOLUTION 2 ###
+
+# import re
+
+# def valid_password(password):
+#     pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@]).{6,12}$')
+
+#     if pattern.match(password):
+#         return "Valid password"
+#     else:
+#         return "Invalid password. Please try again"
+
+# print(valid_password("ABd1234@1"))
