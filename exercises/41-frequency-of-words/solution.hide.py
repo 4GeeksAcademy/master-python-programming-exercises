5adb95e72346e558b008a84b2ae780af885b1d30
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-01 06:24:36 +0100

    Update solution.hide.py

--- exercises/41-frequency-of-words/solution.hide.py
+++ exercises/41-frequency-of-words/solution.hide.py
@@ -1,10 +1,17 @@
-freq = {}   # frequency of words in text
-line = input()
-for word in line.split():
-    freq[word] = freq.get(word,0)+1
+# Your code here
+def compute_word_frequency(sentence):
+    words = sentence.split()
 
-words = freq.keys()
-words.sort()
+    word_frequency = {}
 
-for w in words:
-    print ("%s:%d" % (w,freq[w]))
+    for word in words:
+        word_frequency[word] = word_frequency.get(word, 0) + 1
+
+    sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: x[0])
+
+    for word, frequency in sorted_word_frequency:
+        print(f"{word}: {frequency}")
+
+
+input_sentence = "New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3."
+compute_word_frequency(input_sentence)
