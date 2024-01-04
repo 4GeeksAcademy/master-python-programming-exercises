0c59afb76b21ea9231ec5d12662c28dc7d494dd5
Khrise Franchesca Zegarra Muñoz <129009072+Khrisefzm@users.noreply.github.com>
2024-01-04 15:52:41 +0000

    add '\r\033[K' to delete the input string befor print the result in app.py

--- exercises/002-sum_of_three_numbers/app.py
+++ exercises/002-sum_of_three_numbers/app.py
@@ -1,7 +1,10 @@
 # Sum all three input numbers and print on the console the result
-first_number = int(input("First input"))
-second_number = int(input("Second input"))
-third_number = int(input("Third input"))
-# print here the sum of three inputs
+first_number = int(input("First input: "))
+second_number = int(input("Second input: "))
+third_number = int(input("Third input: "))
 
-print(first_number+second_number)
+# Move the cursor to the beginning of the line and clear the line
+print('\r\033[K', end='')
+
+# print here the sum of three inputs
+print(first_number)
