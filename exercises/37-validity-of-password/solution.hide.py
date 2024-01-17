b95c13dfe61a9223be01cbe755bb1f655d33a58b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-17 17:30:16 +0100

    Update solution.hide.py

--- exercises/37-validity-of-password/solution.hide.py
+++ exercises/37-validity-of-password/solution.hide.py
@@ -4,10 +4,10 @@ import re
 def valid_password(password):
     pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@]).{6,12}$')
     
-    if pattern.match(password):
-        return "Valid password"
-    else:
+    if not pattern.match(password):
         return "Invalid password. Please try again"
+    else:
+        return "Valid password"
 
 
 print(valid_password("ABd1234@1"))
