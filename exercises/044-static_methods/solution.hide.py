9252623a0aa5162951fc4fd67d4de4b65c8eda32
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-30 15:08:04 +0100

    Update solution.hide.py

--- exercises/044-static_methods/solution.hide.py
+++ exercises/044-static_methods/solution.hide.py
@@ -6,7 +6,6 @@ class MathOperations:
     def add_numbers(num1, num2):
         return num1 + num2
 
-math_operations_instance = MathOperations()
 sum_of_numbers = MathOperations.add_numbers(10, 15)
 
 print(f"Sum of Numbers: {sum_of_numbers}")
