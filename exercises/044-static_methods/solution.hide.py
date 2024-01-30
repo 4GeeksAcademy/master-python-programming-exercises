ec29f98af7a8488cbe4fe32c969d2485811bdb6e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-30 15:11:12 +0100

    Update solution.hide.py

--- exercises/044-static_methods/solution.hide.py
+++ exercises/044-static_methods/solution.hide.py
@@ -6,6 +6,7 @@ class MathOperations:
     def add_numbers(num1, num2):
         return num1 + num2
 
+# You can call the static method without creating an instance
 sum_of_numbers = MathOperations.add_numbers(10, 15)
 
 print(f"Sum of Numbers: {sum_of_numbers}")
