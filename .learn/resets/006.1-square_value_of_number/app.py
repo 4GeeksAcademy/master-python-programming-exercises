21347aff51b41964407ca3e08c8e92bea302a5ad
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-12-11 00:39:24 +0000

    7 out of 45 done

--- .learn/resets/006.1-square_value_of_number/app.py
+++ .learn/resets/006.1-square_value_of_number/app.py
@@ -1,5 +1,5 @@
 def square(num):
     # Your code here
-    return num**2
+    return None
 
 print(square(6))
--- .learn/resets/007-hours_and_minutes/app.py
+++ .learn/resets/007-hours_and_minutes/app.py
@@ -1,6 +1,6 @@
 def hours_minutes(seconds):
   # Your code here
-  return (int(seconds/3600), int((seconds%3600)/60))
+  return None
 
 # Invoke the function and pass any integer as its argument
 print(hours_minutes(3900))
